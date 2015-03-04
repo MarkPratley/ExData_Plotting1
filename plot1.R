12a6be3da1c734c36152ea1081d830666b4d14a8
Mark Pratley <markpratley@hotmail.com>
2015-03-04 22:11:52 +0000

    Done

--- plot1.R
+++ plot1.R
@@ -4,7 +4,33 @@
 #       Plot#1
 #        01-Mar-2015
 
-hist(df$GAP,
-     main = "Global Active Power",
-     col="red",
-     xlab = "Global Active Power (kilowatts)" )
+# Outputs a hist into a png
+plot1 <- function(data, file) {
+    
+    # Create  png with filename file
+    png(filename = file, width = 480, height=480)
+    
+    # Create hist
+    hist(data$GAP,
+         main = "Global Active Power",
+         col="red",
+         xlab = "Global Active Power (kilowatts)" )
+    # Close graphics device
+    dev.off()
+}
+
+
+# Start plotting here
+
+source("GetCleanData.R")
+
+#Get the data
+data <- getCleanData()
+
+# Check if dir exists, if not create
+if (! file.exists("./myplots")) {
+    dir.create("./myplots")
+}
+
+# Do the plot bit
+plot1(data, "./myplots/plot1.png")
