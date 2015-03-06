ef3d519b520d915ededa0d99cbb6884105110312
Mark Pratley <markpratley@hotmail.com>
2015-03-06 22:00:47 +0000

    Adding Get file if missing f(x)

--- GetCleanData.R
+++ GetCleanData.R
@@ -4,24 +4,43 @@
 #       Clean Data
 #        01-Mar-2015
 
+# magic url & files names
+urlData <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipData <- "household_power_consumption.zip"
+txtData <- "household_power_consumption.txt"
+
 # Loads relevant .txt rows
 #  Cleans and formats data
 #  Returns data.frame
 getCleanData <- function() {
     
-    df = read.table("household_power_consumption.txt",
+    # Does the data file exists here? If not get it
+    if (!file.exists(txtData))
+        getFile()
+
+    # Read in dataframe
+    df = read.table(txtData,
                     skip=66637,
                     nrows=2880,
                     sep=";",
                     na.strings="?",
-                    col.names=c("Timestamp", "Time", "GAP", "GRP", "Voltage", "GIntensity", "Sub1", "Sub2", "Sub3"))
+                    col.names=c("Timestamp", "Time", "GAP", "GRP", "Voltage",
+                                "GIntensity", "Sub1", "Sub2", "Sub3"))
     
-    # Create Datetime from Date & Time
+    # Create Datetime from Date & Time e.g. "1/2/2007 03:51:00"
     df$Timestamp <- strptime(paste(df$Timestamp, df$Time), format="%d/%m/%Y %H:%M:%S")
     
     # Remove Time col - now we just have a unified Timestamp (Datetime)
     df$Time <- NULL
     
+    # Return
     df
+}
+
+# Downloads & Unzips data to working directory  - called if not already there
+getFile <- function() {
+    
+    download.file(urlData, zipData, method="curl")
+    unzip(zipData, txtData)
 }
 
