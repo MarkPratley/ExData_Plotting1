6cfdb4d16751f69086e2bb7f90003720ae7ddee6
Mark Pratley <markpratley@hotmail.com>
2015-03-06 22:02:40 +0000

    Moved getData f(x)s into each plot file

--- plot3.R
+++ plot3.R
@@ -49,3 +49,46 @@ if (! file.exists("./myplots")) {
 
 # Do the plot bit
 plot3(data, "./myplots/plot3.png")
+
+##----------------------------------------
+# Get and Clean the data functions
+
+# magic url & files names
+urlData <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zipData <- "household_power_consumption.zip"
+txtData <- "household_power_consumption.txt"
+
+# Loads relevant .txt rows
+#  Cleans and formats data
+#  Returns data.frame
+getCleanData <- function() {
+    
+    # Does the data file exists here? If not get it
+    if (!file.exists(txtData))
+        getFile()
+    
+    # Read in dataframe
+    df = read.table(txtData,
+                    skip=66637,
+                    nrows=2880,
+                    sep=";",
+                    na.strings="?",
+                    col.names=c("Timestamp", "Time", "GAP", "GRP", "Voltage",
+                                "GIntensity", "Sub1", "Sub2", "Sub3"))
+    
+    # Create Datetime from Date & Time e.g. "1/2/2007 03:51:00"
+    df$Timestamp <- strptime(paste(df$Timestamp, df$Time), format="%d/%m/%Y %H:%M:%S")
+    
+    # Remove Time col - now we just have a unified Timestamp (Datetime)
+    df$Time <- NULL
+    
+    # Return
+    df
+}
+
+# Downloads & Unzips data to working directory  - called if not already there
+getFile <- function() {
+    
+    download.file(urlData, zipData, method="curl")
+    unzip(zipData, txtData)
+}
